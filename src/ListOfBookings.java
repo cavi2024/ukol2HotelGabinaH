2ea232a208765d64c4d7903f4dc2e98cfc8e355c
Gabriela Horčičková <cavilliera@centrum.cz>
2024-05-27 19:16:57 +0200

    Ukol-2.lekce
    Nový constructor + getter and Setter

--- src/ListOfBookings.java
+++ src/ListOfBookings.java
@@ -7,8 +7,35 @@ public class ListOfBookings {
     private List<Guest> otherGuests;
     private List<ListOfBookings> listOfBookings = new ArrayList<>();
 
-    public ListOfBookings(List<ListOfBookings> listOfBookings) {
+    public ListOfBookings(Room room, Guest guest, List<Guest> otherGuests, List<ListOfBookings> listOfBookings) {
+        this.room = room;
+        this.guest = guest;
+        this.otherGuests = otherGuests;
         this.listOfBookings = listOfBookings;
+    }
+
+    public Room getRoom() {
+        return room;
+    }
+
+    public void setRoom(Room room) {
+        this.room = room;
+    }
+
+    public Guest getGuest() {
+        return guest;
+    }
+
+    public void setGuest(Guest guest) {
+        this.guest = guest;
+    }
+
+    public List<Guest> getOtherGuests() {
+        return otherGuests;
+    }
+
+    public void setOtherGuests(List<Guest> otherGuests) {
+        this.otherGuests = otherGuests;
     }
 
     public List<ListOfBookings> getListOfBookings() {
