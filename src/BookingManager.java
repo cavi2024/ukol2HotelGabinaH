69010bcff7ddd4cf6466e545bb50b63287ffedfd
Gabriela Horčičková <cavilliera@centrum.cz>
2024-06-04 20:39:58 +0200

    Ukol-3.lekce
    Oprava řádku 41 v BookingManager z isWorkingTypeOfVacation na getWorkingTypeOfVacation

--- src/BookingManager.java
+++ src/BookingManager.java
@@ -1,3 +1,4 @@
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -5,28 +6,57 @@ public class BookingManager {
     private Room room;
     private Guest guest;
     private List<Guest> otherGuests;
-    private List<Booking> listBooking;
+    private List<Booking> listBooking = new ArrayList<>();
 
     public BookingManager(Room room, Guest guest, List<Guest> otherGuests, List<Booking> listBooking) {
         this.room = room;
         this.guest = guest;
         this.otherGuests = otherGuests;
-        this.listBooking = listBooking;
+        this.listBooking.addAll(listBooking);
     }
 
     public void addBooking(Booking newBooking) {
-        listBooking.add(newBooking);
+        listBooking.add(newBooking); // Vložení další rezervace do seznamu
     }
 
     public Booking getBooking(int index) {
-        return listBooking.get(index);
+        return listBooking.get(index); // Získání rezervace se zadaným indexem ze seznamu
     }
 
     public Booking getBookings() {
-        return getBookings();
+        return getBookings(); // Získání seznamu rezervací - vyměň za getBookinds co má vrátit
     }
 
     public void clearBookings() {
+        listBooking.clear();
+    }
+
+    // public boolean isWorkingTypeOfVacation () {
+    //     return isWorkingTypeOfVacation();
+    //}
+
+    public int getNumberOfWorkingBookings() {
+        int numberOfWorkingBookings = 0;
+        for (Booking booking : listBooking) {
+            if (!booking.getWorkingTypeOfVacation()) {
+                numberOfWorkingBookings++;
+            }
+        }
+        return numberOfWorkingBookings;
+    }
+
+    public int getNumberOfGuests() {
+        int numberOfGuests = 0;
+        for (Booking booking : listBooking) { // správně?
+        }
+        return numberOfGuests;
+    }
+
+    public BigDecimal getAverageGuests() {
+        BigDecimal averageGuests = BigDecimal.valueOf(0);
+        for (Booking booking : listBooking) { //nutno dopsat spočítání průměru
+        }
+        return averageGuests;
     }
 
     public Room getRoom() {
@@ -54,7 +84,7 @@ public class BookingManager {
     }
 
     public List<Booking> getListBooking() {
-        return listBooking;
+        return new ArrayList<>(listBooking); // kopie seznamu rezervací
     }
 
     public void setListBooking(List<Booking> listBooking) {
