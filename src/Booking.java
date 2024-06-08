33602a37f64edfe0bb1ace133084fbc6f2a2ba00
Gabriela Horčičková <cavilliera@centrum.cz>
2024-06-08 22:17:00 +0200

    Ukol-3.lekce
    Přidání List<Guest> guests  + změna v konstruktorech a v Getter and Setter

--- src/Booking.java
+++ src/Booking.java
@@ -7,24 +7,24 @@ public class Booking {
     private LocalDate lastDay;
     private Boolean isWorkingTypeOfVacation;
     private Room room;
-    private Guest guest;
+    private List<Guest> guests;
     private List<Guest> otherGuests;
 
-    public Booking(LocalDate firstDay, LocalDate lastDay, Boolean isWorkingTypeOfVacation, Room room, Guest guest, List<Guest> otherGuests) {
+    public Booking(LocalDate firstDay, LocalDate lastDay, Boolean isWorkingTypeOfVacation, Room room, List<Guest> guests, List<Guest> otherGuests) {
         this.firstDay = firstDay;
         this.lastDay = lastDay;
         this.isWorkingTypeOfVacation = isWorkingTypeOfVacation;
         this.room = room;
-        this.guest = guest;
+        this.guests = guests;
         this.otherGuests = otherGuests;
     }
 
-    public Booking(LocalDate firstDay, LocalDate lastDay, Boolean isWorkingTypeOfVacation, Room room, Guest guest) {
+    public Booking(LocalDate firstDay, LocalDate lastDay, Boolean isWorkingTypeOfVacation, Room room, List<Guest> guests) {
         this.firstDay = firstDay;
         this.lastDay = lastDay;
         this.isWorkingTypeOfVacation = isWorkingTypeOfVacation;
         this.room = room;
-        this.guest = guest;
+        this.guests = guests;
     }
 
     public LocalDate getFirstDay() {
@@ -59,12 +59,12 @@ public class Booking {
         this.room = room;
     }
 
-    public Guest getGuest() {
-        return guest;
+    public List<Guest> getGuests() {
+        return guests;
     }
 
-    public void setGuest(Guest guest) {
-        this.guest = guest;
+    public void setGuests(List<Guest> guests) {
+        this.guests = guests;
     }
 
     public List<Guest> getOtherGuests() {
@@ -75,6 +75,10 @@ public class Booking {
         this.otherGuests = otherGuests;
     }
 
+    public int getNumberOfGuests() {
+        return guests.size();
+    }
+
     @Override
     public String toString() {
         return "Booking{" +
@@ -82,7 +86,7 @@ public class Booking {
                 ", lastDay=" + lastDay +
                 ", isWorkingTypeOfVacation=" + isWorkingTypeOfVacation +
                 ", room=" + room +
-                ", guest=" + guest +
+                ", guest=" + guests +
                 ", otherGuests=" + otherGuests +
                 '}';
     }
