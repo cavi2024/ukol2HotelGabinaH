92862942ac99583a7f026dad53e30079cf9a6d00
Gabriela Horčičková <cavilliera@centrum.cz>
2024-06-29 13:30:49 +0200

    listOfBooking místo listBookingManager
    Přejmenování listBookingManager na listOfBookings

--- src/BookingManager.java
+++ src/BookingManager.java
@@ -4,49 +4,49 @@ import java.util.List;
 
 public class BookingManager {
 
-    private List<Booking> listBookingManager = new ArrayList<>();
+    private List<Booking> listOfBookings = new ArrayList<>();
 
 
-    public BookingManager(List<Booking> listBookingManager) {
-        this.listBookingManager.addAll(listBookingManager);
+    public BookingManager(List<Booking> listOfBookings) {
+        this.listOfBookings.addAll(listOfBookings);
     }
 
     public BookingManager() {}
 
 
     // Metody přidané z úkolu:
-    public List<Booking> getListBookingManager() {
-        return new ArrayList<>(listBookingManager); // kopie seznamu rezervací
+    public List<Booking> getListOfBookings() {
+        return new ArrayList<>(listOfBookings); // kopie seznamu rezervací
     }
 
-    public void setListBookingManager(List<Booking> listBookingManager) {
-        this.listBookingManager = listBookingManager;
+    public void setListOfBookings(List<Booking> listOfBookings) {
+        this.listOfBookings = listOfBookings;
     }
 
     public void addBooking(Booking newBooking) {
-        listBookingManager.add(newBooking); // Vložení další rezervace do seznamu
+        listOfBookings.add(newBooking); // Vložení další rezervace do seznamu
     }
 
     public Booking getBooking(int index) {
-        return listBookingManager.get(index); // Získání rezervace se zadaným indexem ze seznamu
+        return listOfBookings.get(index); // Získání rezervace se zadaným indexem ze seznamu
     }
 
     public void addList(List<Booking> list) {
-        listBookingManager.addAll(list);
+        listOfBookings.addAll(list);
     }
 
     public List<Booking> getBookings() {
-        return new ArrayList<>(listBookingManager); // Získání seznamu rezervací -  co má vrátit
+        return new ArrayList<>(listOfBookings); // Získání seznamu rezervací -  co má vrátit
     }
 
     public void clearBookings() {
-        listBookingManager.clear();
+        listOfBookings.clear();
     }
     // Počet pracovních rezervací:
 
     public int getNumberOfWorkingBookings() {
         int numberOfWorkingBookings = 0;
-        for (Booking booking : listBookingManager) {
+        for (Booking booking : listOfBookings) {
             if (!booking.getWorkingTypeOfVacation()) {
                 numberOfWorkingBookings++;
             }
@@ -58,11 +58,11 @@ public class BookingManager {
     public BigDecimal getAverageGuests() {
         BigDecimal averageGuests = BigDecimal.valueOf(0);
         BigDecimal sumOfGuests = BigDecimal.valueOf(0);
-        for (Booking booking : listBookingManager) {
+        for (Booking booking : listOfBookings) {
             sumOfGuests = sumOfGuests.add(BigDecimal.valueOf(booking.getNumberOfGuests()));
         }
-        if (listBookingManager.size() > 0) {
-            averageGuests = sumOfGuests.divide(BigDecimal.valueOf(listBookingManager.size()));
+        if (listOfBookings.size() > 0) {
+            averageGuests = sumOfGuests.divide(BigDecimal.valueOf(listOfBookings.size()));
         }
         return averageGuests;
     }
